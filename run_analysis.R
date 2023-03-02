b7f59a519d36850146a8aadfdc76d806f4eedefb
Holly Frost <frost.holly.e@gmail.com>
2023-03-02 14:44:57 -0800

    Updated to write the datat to txt fiels

--- run_analysis.R
+++ run_analysis.R
@@ -1,10 +1,10 @@
-#Unzips the file
+#Unzip the files
 unzip("getdata_projectfiles_UCI HAR Dataset.zip")
 
 #Load packages
 library(dplyr)
 
-#Read test data, train data, data labels, activity labels
+#Read: test data, train data, data labels, activity labels
 test <- tibble(read.table("UCI HAR Dataset/test/X_test.txt"))
 train <- tibble(read.table("UCI HAR Dataset/train/X_train.txt"))
 labels <- tibble(read.table("UCI HAR Dataset/features.txt"))
@@ -14,22 +14,22 @@ activity_labels <-tibble(read.table("UCI HAR Dataset/activity_labels.txt"))
 test_act <- tibble(read.table("UCI HAR Dataset/test/Y_test.txt"))
 train_act <- tibble(read.table("UCI HAR Dataset/train/Y_train.txt"))
 
-#Merges data test and train data
+#Merge data test and train data
 all_data <- rbind(test,train)
 remove(train,test)
 
-#Selects columns which contain 'mean' or 'std'
+#Select columns containing 'mean' or 'std' in label data
 my_patterns <- c('[Mm]ean', '[Ss]td')
 my_cols <- grepl(paste(my_patterns, collapse='|'),labels$V2)
 select_labels <- filter(labels,my_cols)
 remove(my_col,my_patterns)
 
-#Selects the data containing 'mean' or 'std'
+#Select the data containing 'mean' or 'std'
 select_data <- all_data %>% 
   select(names(all_data)[my_cols])
 remove(all_data,my_cols)
 
-#Renames select data
+#Rename select data
 select_data <- rename_with(select_data, ~select_labels$V2,
                            all_of(colnames(select_data)))
 remove(labels,select_labels)
@@ -40,7 +40,7 @@ select_data <- add_row(test_subject,train_subject) %>%
   rename(subject=V1)
 remove(test_subject,train_subject)
 
-#Add activity data
+#Add activity data with tidy name
 act <- rbind(test_act,train_act)
 assign_activity <- function(activity) {
   if(activity ==1) {x = "WALKING"}
@@ -57,3 +57,7 @@ remove(activity_labels,test_act,train_act,assign_activity,act)
 #Group by subject and activity
 select_data <- group_by(select_data, subject, activity)
 grouped <- summarize_all(select_data, mean)
+
+#Creates file containing grouped data and select data
+write.table(select_data,"select_data.txt")
+write.table(grouped,"grouped.txt")
