048bc83691005fb3aa6c17dadd5c195f57f1e425
Holly Frost <frost.holly.e@gmail.com>
2023-03-02 13:46:15 -0800

    Updates the README.md file. Uploads the raw data zip file. Uploads the R project file. Uploads the run_analysis.R file which contains the analysis.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # getting_and_cleaning_data_final
 This is my final project for JH Coursera Course - Getting and Cleaning Data.
+
+This analysis:
+1. Combines the test and train data sets
+2. Selects the mean and standard deviation data from the set.
+3. 
---
+++ run_analysis.R
@@ -0,0 +1,59 @@
+#Unzips the file
+unzip("getdata_projectfiles_UCI HAR Dataset.zip")
+
+#Load packages
+library(dplyr)
+
+#Read test data, train data, data labels, activity labels
+test <- tibble(read.table("UCI HAR Dataset/test/X_test.txt"))
+train <- tibble(read.table("UCI HAR Dataset/train/X_train.txt"))
+labels <- tibble(read.table("UCI HAR Dataset/features.txt"))
+test_subject <-tibble(read.table("UCI HAR Dataset/test/subject_test.txt"))
+train_subject <-tibble(read.table("UCI HAR Dataset/train/subject_train.txt"))
+activity_labels <-tibble(read.table("UCI HAR Dataset/activity_labels.txt"))
+test_act <- tibble(read.table("UCI HAR Dataset/test/Y_test.txt"))
+train_act <- tibble(read.table("UCI HAR Dataset/train/Y_train.txt"))
+
+#Merges data test and train data
+all_data <- rbind(test,train)
+remove(train,test)
+
+#Selects columns which contain 'mean' or 'std'
+my_patterns <- c('[Mm]ean', '[Ss]td')
+my_cols <- grepl(paste(my_patterns, collapse='|'),labels$V2)
+select_labels <- filter(labels,my_cols)
+remove(my_col,my_patterns)
+
+#Selects the data containing 'mean' or 'std'
+select_data <- all_data %>% 
+  select(names(all_data)[my_cols])
+remove(all_data,my_cols)
+
+#Renames select data
+select_data <- rename_with(select_data, ~select_labels$V2,
+                           all_of(colnames(select_data)))
+remove(labels,select_labels)
+
+#Add subject data with tidy name
+select_data <- add_row(test_subject,train_subject) %>%
+  mutate(select_data) %>%
+  rename(subject=V1)
+remove(test_subject,train_subject)
+
+#Add activity data
+act <- rbind(test_act,train_act)
+assign_activity <- function(activity) {
+  if(activity ==1) {x = "WALKING"}
+  else if(activity == 2) {"WALKING_UPSTAIRS"}
+  else if(activity == 3) {"WALKING_DOWNSTAIRS"}
+  else if(activity == 4) {"SITTING"}
+  else if(activity == 5) {"STANDING"}
+  else {"LAYING"}
+}
+act <- sapply(act$V1,assign_activity)
+select_data <- mutate(select_data,activity = act,.after=subject)
+remove(activity_labels,test_act,train_act,assign_activity,act)
+
+#Group by subject and activity
+select_data <- group_by(select_data, subject, activity)
+grouped <- summarize_all(select_data, mean)
